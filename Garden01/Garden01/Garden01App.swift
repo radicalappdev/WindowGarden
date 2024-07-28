a3b35a81ae69aff5669770729b656e5b847bf094
Joseph Simpson <joe@radicalappdev.com>
2024-07-28 06:07:03 -0400

    Adding garden 01

---
+++ Garden01/Garden01/Garden01App.swift
@@ -0,0 +1,33 @@
+//
+//  Garden01App.swift
+//  Garden01
+//
+//  Created by Joseph Simpson on 7/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Garden01App: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.full), in: .full)
+    }
+}
---
+++ Garden01/Garden01/ImmersiveView.swift
@@ -0,0 +1,30 @@
+//
+//  ImmersiveView.swift
+//  Garden01
+//
+//  Created by Joseph Simpson on 7/28/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ImmersiveView: View {
+
+    var body: some View {
+        RealityView { content in
+            // Add the initial RealityKit content
+            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
+                content.add(immersiveContentEntity)
+
+                // Put skybox here.  See example in World project available at
+                // https://developer.apple.com/
+            }
+        }
+    }
+}
+
+#Preview(immersionStyle: .full) {
+    ImmersiveView()
+        .environment(AppModel())
+}
