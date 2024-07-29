a8f4eb96db09de41cc60d2550abce3f502beb117
Joseph Simpson <joe@radicalappdev.com>
2024-07-29 14:13:46 -0400

    windowResizability examples

--- Garden02/Garden02/Garden02App.swift
+++ Garden02/Garden02/Garden02App.swift
@@ -17,6 +17,11 @@ struct Garden02App: App {
 
         WindowGroup(id: "YellowFlower") {
             YellowFlowerView()
+                .frame(minWidth: 500, maxWidth: 700, minHeight: 500, maxHeight: 700)
+            // .frame(width: 600, height: 600) // used to set a fized size
         }
+        .windowResizability(.contentSize)
         .defaultSize(CGSize(width: 600, height: 600))
-    }}
+
+    }
+}
