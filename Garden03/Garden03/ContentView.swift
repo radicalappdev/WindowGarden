1f8c63cf89cd9a5fc5c44267dd24dd62d7a54ca1
Joseph Simpson <joe@radicalappdev.com>
2024-07-30 18:15:08 -0400

    Starting Garden 03

---
+++ Garden03/Garden03/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Garden03
+//
+//  Created by Joseph Simpson on 7/30/24.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+
+    var body: some View {
+        VStack {
+            Model3D(named: "Scene", bundle: realityKitContentBundle)
+                .padding(.bottom, 50)
+
+            Text("Hello, world!")
+
+        }
+        .padding()
+    }
+}
+
+#Preview(windowStyle: .automatic) {
+    ContentView()
+}
---
+++ Garden03/Garden03/Garden03App.swift
@@ -0,0 +1,20 @@
+//
+//  Garden03App.swift
+//  Garden03
+//
+//  Created by Joseph Simpson on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Garden03App: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+
+        }
+
+    }
+}
